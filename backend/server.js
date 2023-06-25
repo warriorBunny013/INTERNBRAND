fc88e6c0abb60cb9ca061f93be0cc6bdd35a9a93
warriorBunny013 <mona23sonai@gmail.com>
2023-06-25 22:32:36 +0530

    added deployed url

--- backend/server.js
+++ backend/server.js
@@ -9,7 +9,7 @@ const app=express();
 app.use(express.json());
 app.use(express.urlencoded({extended:false}))
 app.use(cors({
-  origin:["http://localhost:3000"],
+  origin:["https://internbrand-uditi-frontend.onrender.com"],
   methods:["POST","GET","PATCH","PUT","DELETE"],
   credentials:true
 }));
--- frontend/src/Api/api.js
+++ frontend/src/Api/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 axios.defaults.withCredentials=true;
-const url = 'http://localhost:8800/api';
+const url = 'https://internbrand-uditi-backend.onrender.com/api';
 
 export const getDatas= async () => {
     return await axios.get(`${url}/get`).catch((err)=>console.log(err));
